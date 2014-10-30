fef4517b2bc184fded1fdc95aea4cee37bf58bcf
worstboy32 <clashwars@hotmail.com>
2014-10-30 14:18:34 +0100

    %Fix compile error. Can't use diamond operators.

--- src/main/java/com/clashwars/cwcore/utils/CWUtil.java
+++ src/main/java/com/clashwars/cwcore/utils/CWUtil.java
@@ -794,7 +794,7 @@ public class CWUtil {
     }
 
     public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue( Map<K, V> map ) {
-        List<Map.Entry<K, V>> list = new LinkedList<>(map.entrySet());
+        List<Map.Entry<K, V>> list = new LinkedList<Map.Entry<K, V>>(map.entrySet());
         Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
             @Override
             public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
@@ -802,7 +802,7 @@ public class CWUtil {
             }
         } );
 
-        Map<K, V> result = new LinkedHashMap<>();
+        Map<K, V> result = new LinkedHashMap<K, V>();
         for (Map.Entry<K, V> entry : list) {
             result.put(entry.getKey(), entry.getValue());
         }
