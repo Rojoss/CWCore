5d9680061c9ee568c976e5bdb8eb8da7bd72cb85
worstboy32 <clashwars@hotmail.com>
2014-11-09 20:03:21 +0100

    %Remake CircleEffect

--- src/main/java/com/clashwars/cwcore/effect/effects/CircleEffect.java
+++ src/main/java/com/clashwars/cwcore/effect/effects/CircleEffect.java
@@ -4,21 +4,15 @@ import com.clashwars.cwcore.effect.Effect;
 import com.clashwars.cwcore.effect.EffectManager;
 import com.clashwars.cwcore.effect.EffectType;
 import com.clashwars.cwcore.packet.ParticleEffect;
-import com.clashwars.cwcore.utils.VectorUtils;
 import org.bukkit.Location;
 import org.bukkit.util.Vector;
 
 /**
- * Creates a circle that can orbit around.<br>
+ * Creates a basic circle.<br>
  * <b>DEFAULTS:</b><br>
  * particle = ParticleEffect.HAPPY_VILLAGER<br>
- * itemID = xRotation, yRotation, zRotation = 0<br>
- * radius = .4f<br>
- * particles = 20<br>
- * <br>
- * type = EffectType.REPEATING<br>
- * period = 4<br>
- * iterations = 25<br>
+ * diameter = .4f<br>
+ * stepSize = 5<br>
  */
 public class CircleEffect extends Effect {
 
@@ -28,49 +22,29 @@ public class CircleEffect extends Effect {
     public ParticleEffect particle = ParticleEffect.HAPPY_VILLAGER;
 
     /*
-     * Rotation of the torus.
+     * Radius of circle
      */
-    public double xRotation, yRotation, zRotation = 0;
+    public float diameter = .4f;
 
     /*
-     * Radius of circle above head
+     * Degrees to increase per itteration.
      */
-    public float radius = .4f;
+    public int stepSize = 5;
 
-    /*
-     * Current step. Works as a counter
-     */
-    protected float step = 0;
-
-    /*
-     * Subtracts from location if needed
-     */
-    public double xSubtract, ySubtract, zSubtract;
-
-    /*
-     * Amount of particles per circle
-     */
-    public int particles = 20;
 
     public CircleEffect(EffectManager effectManager) {
         super(effectManager);
         type = EffectType.INSTANT;
-        period = 1;
-        iterations = 200;
     }
 
     @Override
     public void onRun() {
-        Location location = getLocation();
-        location.subtract(xSubtract, ySubtract, zSubtract);
-        double inc = (2*Math.PI)/particles;
-        double angle = step * inc;
-        Vector v = new Vector();
-        v.setX(radius * Math.cos(angle));
-        v.setZ(radius * Math.sin(angle));
-        VectorUtils.rotateVector(v, xRotation, yRotation, zRotation);
-        particle.display(location.add(v), visibleRange, (float)particleOffset.getX(), (float)particleOffset.getY(), (float)particleOffset.getZ(), speed, amt);
-        step++;
+        for (int step = 0; step <= 360; step += stepSize) {
+            Vector v = new Vector();
+            v.setX(diameter * Math.cos(step));
+            v.setZ(diameter * Math.sin(step));
+            Location location = getLocation().clone();
+            particle.display(location.add(v).add(((double)diameter/2)-1, 0, ((double)diameter/2)-1), visibleRange, (float)particleOffset.getX(), (float)particleOffset.getY(), (float)particleOffset.getZ(), speed, amt);
+        }
     }
-
 }
